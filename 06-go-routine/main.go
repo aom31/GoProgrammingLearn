b4f00ef7b9381d252dd89b398f2b7ff3c84d9e54
Thamakorn Ketnoi <thamakorn.k@siriustech.io>
2023-03-05 15:19:44 +0700

    end go routine

--- 06-go-routine/main.go
+++ 06-go-routine/main.go
@@ -32,8 +32,35 @@ func main() {
 
 
 	// 2# send data from main routine to go channel all
+	existChannelFromMain := make(chan bool)
+	for i:=0;i<workerCount;i++{
+		workerID := fmt.Sprintf("worker-%v",i)
+		go worker3(workerID, existChannelFromMain)
+	}
+	time.Sleep(10* time.Second)
+	for i:=0;i< workerCount;i++{
+		existChannelFromMain <- true
+	}
+	close(existChannelFromMain)
+	time.Sleep(2*time.Second)
+	fmt.Println("Main channel routine is exited")
 
 
+}
+
+func worker3(workerID string, existChannelFromMain chan bool){
+	i := 0
+	for  {
+		i++
+		select {
+		case <- existChannelFromMain :
+				fmt.Println(fmt.Sprintf(" worker-%v has exited",workerID))
+				return
+		default:
+			fmt.Println(fmt.Sprintf("woker= %v, couter=%v", workerID,i))
+			time.Sleep(1*time.Second)	
+		}
+	}
 }
 
 func worker1(workerID string, responseChannel chan string) {
